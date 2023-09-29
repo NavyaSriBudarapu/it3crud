a3a7272f3438be540992fd9530e30801746efb90
NavyaSriBudarapu <navyasribudarapu@gmail.com>
2023-09-29 10:28:39 +0530

    Initial commit

---
+++ server/app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const url = 'mongodb+srv://navyasribudarapu:cbit@cluster0.1ihg4yn.mongodb.net/?retryWrites=true&w=majority/CBIT'
+const app = express()
+mongoose.connect(url, { useNewUrlParser: true })
+const con = mongoose.connection
+con.on('open', () => {
+    console.log('connected...')
+})
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.json())
+const alienRouter = require('./routes/aliens')
+app.use('/aliens', alienRouter)
+app.listen(9000, () => {
+    console.log('Server started')
+})
